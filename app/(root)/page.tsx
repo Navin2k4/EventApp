faa28fdaac37ed74cb529617c38480e48e0c7af3
Navin Kumaran <navinkumaran2004@gmail.com>
2024-06-25 21:43:14 +0530

    Home page Display and Edit Delete Functionality added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,19 @@
+import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/lib/actions/event.action";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+
+  const events = await getAllEvents(
+    {
+      query : '',
+      category : '',
+      page : 1,
+      limit : 6
+    });
+    
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -26,12 +37,24 @@ export default function Home() {
           />
         </div>
       </section>
+
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
         <h2 className="h2-bold">Trusted By <br/>Thoushands of Events</h2>
         <div className="flex w-full flex-col gap-5 md:flex-row">
           Search
           Categories
         </div>
+      
+        <Collection 
+          data = {events?.data}
+          emptyTitle="No events Found"
+          emptyStateSubtext="Come back Later"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2}
+        />
+
       </section> 
     </>
   );
